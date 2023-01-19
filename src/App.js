385a650f8a8ebe7dc85c789333934e14946eabd7
Yeonuk Hwang <younuk23@gmail.com>
2023-01-19 23:44:31 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => console.log("render"));
+  useEffect(() => console.log("count changed: ", count), [count]);
+  useEffect(() => console.log("toggle changed: ", toggle), [toggle]);
+  useEffect(
+    () => console.log("count or toggle changed:", count, toggle),
+    [count, toggle]
+  );
+  useEffect(() => console.log("---------"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>count:{count}</h1>
+      <button onClick={() => setCount(count + 1)}>up</button>
+      <h1>current Toggle:{toggle.toString()}</h1>
+      <button onClick={() => setToggle(!toggle)}>toggle</button>
     </div>
   );
 }
